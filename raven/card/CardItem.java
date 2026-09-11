10117107ab85f518e8f27e80f60b1b5878457c70
Mohamad Abdallah <hamadakakos@gmail.com>
2026-09-11 17:19:43 -0300

    Force the the colors to the cardbox

--- raven/card/CardItem.java
+++ raven/card/CardItem.java
@@ -9,56 +9,56 @@ import java.awt.*;
 
 public class CardItem extends JPanel {
 
-    public CardItem(Icon icon, String title) {
-        init(icon, title);
-    }
+	private final String redB = "arc:8;" + "border:2,5,2,5;" + "background:fade(#f43f5e,10%);"
+			+ "foreground:#f43f5e;",
+			greenB = "arc:8;" + "border:2,5,2,5;" + "background:fade(#1f9669,10%);"
+					+ "foreground:#1f9669;";
 
-    private void init(Icon icon, String title) {
-        setLayout(new MigLayout("hidemode 3,wrap", "15 push[] 15 push"));
-        putClientProperty(FlatClientProperties.STYLE, "" +
-                "background:null;");
+	public CardItem(Icon icon, String title) {
+		init(icon, title);
+	}
 
-        lbTitle = new JLabel(title, icon, JLabel.LEADING);
-        lbValue = new JLabel("0");
-        lbDescription = new JLabel("description");
-        lbTags = new JLabel("0%");
+	private void init(Icon icon, String title) {
+		setLayout(new MigLayout("hidemode 3,wrap", "15 push[] 15 push"));
+		putClientProperty(FlatClientProperties.STYLE, "" + "background:null;");
 
-        lbTitle.putClientProperty(FlatClientProperties.STYLE, "" +
-                "foreground:$Label.disabledForeground;");
-        lbValue.putClientProperty(FlatClientProperties.STYLE, "" +
-                "font:+8;");
+		lbTitle = new JLabel(title, icon, JLabel.LEADING);
+		lbValue = new JLabel("0");
+		lbDescription = new JLabel("description");
+		lbTags = new JLabel("0%");
 
-        lbDescription.putClientProperty(FlatClientProperties.STYLE, "" +
-                "foreground:$Label.disabledForeground;");
+		lbTitle.putClientProperty(FlatClientProperties.STYLE, "" + "foreground:$Label.disabledForeground;");
+		lbValue.putClientProperty(FlatClientProperties.STYLE, "" + "font:+8;");
 
-        lbTags.putClientProperty(FlatClientProperties.STYLE_CLASS, "greenBadge small");
+		lbDescription.putClientProperty(FlatClientProperties.STYLE, "" + "foreground:$Label.disabledForeground;");
+		
+		lbTags.putClientProperty(FlatClientProperties.STYLE, greenB);
 
-        add(lbTitle);
-        add(lbValue);
-        add(lbDescription, "split 2");
-        add(lbTags);
-    }
+		add(lbTitle);
+		add(lbValue);
+		add(lbDescription, "split 2");
+		add(lbTags);
+	}
 
-    public void setValue(String value, String description, String tags, boolean up) {
-        lbValue.setText(value);
-        lbDescription.setText(description);
-        lbTags.setText(tags);
-        lbTags.putClientProperty(FlatClientProperties.STYLE_CLASS, (up ? "greenBadge" : "redBadge") + " small");
-        lbDescription.setVisible(description != null);
-        lbTags.setVisible(tags != null);
-    }
+	public void setValue(String value, String description, String tags, boolean up) {
+		lbValue.setText(value);
+		lbDescription.setText(description);
+		lbTags.setText(tags);
+		lbTags.putClientProperty(FlatClientProperties.STYLE, up ? greenB : redB);
+		lbDescription.setVisible(description != null);
+		lbTags.setVisible(tags != null);
+	}
 
+	public void setCardIconColor(Color color) {
+		if (lbTitle.getIcon() != null && lbTitle.getIcon() instanceof FlatSVGIcon) {
+			FlatSVGIcon icon = (FlatSVGIcon) lbTitle.getIcon();
+			icon.getColorFilter().setMapper(_ -> color);
+			repaint();
+		}
+	}
 
-    public void setCardIconColor(Color color) {
-        if (lbTitle.getIcon() != null && lbTitle.getIcon() instanceof FlatSVGIcon) {
-            FlatSVGIcon icon = (FlatSVGIcon) lbTitle.getIcon();
-            icon.getColorFilter().setMapper(_ -> color);
-            repaint();
-        }
-    }
-
-    private JLabel lbTitle;
-    private JLabel lbValue;
-    private JLabel lbDescription;
-    private JLabel lbTags;
+	private JLabel lbTitle;
+	private JLabel lbValue;
+	private JLabel lbDescription;
+	private JLabel lbTags;
 }
